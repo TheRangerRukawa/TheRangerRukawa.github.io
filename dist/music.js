3b14192b7f5e29374de93d9db79158b2c4f75794
song <smile7shine@163.com>
2020-05-17 14:38:16 +0800

    Site updated: 2020-05-17 14:38:16

--- dist/music.js
+++ dist/music.js
@@ -4,18 +4,36 @@ const ap = new APlayer({
     autoplay: true,
     audio: [
 	  {
-        name: 'Wind Of Change',
-        artist: 'Scorpions',
-        url: 'http://m701.music.126.net/20200517143013/550f3d6587ec4597921063ee62280529/jdyyaac/010f/065c/0e5e/6e13b49ef4164c1c13b39483b5093736.m4a',
-        cover: 'http://p1.music.126.net/k5OMDkOKYi-5lfwuycZ38w==/780653266000422.jpg',
+        name: '日落大道',
+        artist: '梁博',
+        url: 'http://m701.music.126.net/20200517145838/964a54ef26d9d253dd5ac83d6b103b31/jdyyaac/5509/565b/565c/bc50000c7fa1ea090aa6a6c99795687b.m4a',
+        cover: 'http://p1.music.126.net/bkBg46eD1bS9D2mzxnKAnQ==/3395291910036707.jpg',
+      },	
+	  {
+        name: '九月(海子)',
+        artist: '周云蓬',
+        url: 'http://m701.music.126.net/20200517150107/853cfb5b130ca64a18e6c15e162a099d/jdyyaac/565d/565c/535a/a7d10b75b8a6d57670fed028cd271038.m4a',
+        cover: 'http://p1.music.126.net/YC6MRePLPuU4UT3KCt8C8Q==/2379343162564454.jpg',
       },
-	  
       {
         name: 'Посвящение',
         artist: 'Vitas',
         url: 'http://m701.music.126.net/20200517145350/978ce42ab29ea30575bf1bae6eb78584/jdyyaac/045f/515c/015f/353d3b9b2df64946c534659f3c12cf32.m4a',
         cover: 'http://p1.music.126.net/eDQJ4rTAghJyJlxbyZ5Ddg==/1652565976548780.jpg',
       },
+	  {
+        name: 'Wind Of Change',
+        artist: 'Scorpions',
+        url: 'http://m701.music.126.net/20200517143013/550f3d6587ec4597921063ee62280529/jdyyaac/010f/065c/0e5e/6e13b49ef4164c1c13b39483b5093736.m4a',
+        cover: 'http://p1.music.126.net/k5OMDkOKYi-5lfwuycZ38w==/780653266000422.jpg',
+      },
+	  {
+        name: 'Still Loving You',
+        artist: 'Scorpions',
+        url: 'http://m10.music.126.net/20200517145727/7a8db5658351ec60c47209bfc6073497/ymusic/0e53/5152/5153/99e3aa00e9f8af3c0e71f9d75bdafe55.mp3',
+        cover: 'http://p1.music.126.net/-1s_sb1Z49-EW2q_8Abv8Q==/109951163444414775.jpg',
+      },	  
+
     ]
 });
 
