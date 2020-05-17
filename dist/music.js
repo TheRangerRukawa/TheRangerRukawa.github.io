f195305f492151689ece4a884897f07c08657ef8
song <smile7shine@163.com>
2020-05-17 14:30:53 +0800

    Site updated: 2020-05-17 14:30:53

---
+++ dist/music.js
@@ -0,0 +1,22 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+	  {
+        name: 'Wind Of Change',
+        artist: 'Scorpions',
+        url: 'http://m701.music.126.net/20200517143013/550f3d6587ec4597921063ee62280529/jdyyaac/010f/065c/0e5e/6e13b49ef4164c1c13b39483b5093736.m4a',
+        cover: 'http://p1.music.126.net/k5OMDkOKYi-5lfwuycZ38w==/780653266000422.jpg',
+      },
+	  
+      {
+        name: 'Посвящение',
+        artist: 'Vitas',
+        url: 'http://m701.music.126.net/20200517145350/978ce42ab29ea30575bf1bae6eb78584/jdyyaac/045f/515c/015f/353d3b9b2df64946c534659f3c12cf32.m4a',
+        cover: 'http://p1.music.126.net/eDQJ4rTAghJyJlxbyZ5Ddg==/1652565976548780.jpg',
+      },
+    ]
+});
+
+
